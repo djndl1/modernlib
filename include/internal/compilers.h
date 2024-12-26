e9a0b384951be69aff652fd538f37913de36dee1
djndl1 <djndl1@gmail.com>
2024-12-26 22:32:42 +0800

    fix(msvc): fix incorrect spelling of MSVC import/export annotations

--- include/internal/compilers.h
+++ include/internal/compilers.h
@@ -11,11 +11,11 @@
 
 #if defined(_MSC_VER) || defined(__MINGW32__) || defined(__MINGW32__)
 #  if defined(MODERNLIB_SHARED_BUILD)
-#     define MODERNLIB_PUBLIC declspec(dllexport)
+#     define MODERNLIB_PUBLIC __declspec(dllexport)
 #  elif defined(MODERNLIB_STATIC_BUILD)
 #     define MODERNLIB_PUBLIC
 #  else
-#     define MODERNLIB_PUBLIC declspec(dllimport)
+#     define MODERNLIB_PUBLIC __declspec(dllimport)
 #  endif
 #elif defined(__GNUC__)
 #  define MODERNLIB_PUBLIC __attribute__((visibility("default")))
