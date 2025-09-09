ed0802325cf7762cfe773194d98f5412f4d97c13
djndl1 <djndl1@gmail.com>
2025-09-09 22:56:15 +0800

    test(utcconverter): use new u16 string functions in replace of wcs* functions

--- test/utfconverter_test.c
+++ test/utfconverter_test.c
@@ -1,54 +1,63 @@
+#include <uchar.h>
 #include <wchar.h>
 #include "modernlib/basis.h"
+#include "modernlib/u16char.h"
 #include "modernlib/utfconverter.h"
 
 #include"utest.h"
 
+UTEST(UTFETest, BasicAssumptions) {
+    // not guaranteed by the standard, I assume so
+    ASSERT_EQ(4, sizeof(char32_t));
+    ASSERT_EQ(2, sizeof(char16_t));
+}
+
 UTEST(UTFTest, NormalUTF8ToUTF16) {
-    const wchar_t utf16_str[] = L"中文ABCD";
+    const char16_t utf16_str[] = u"中文ABCD";
     const char *utf8= u8"中文ABCD";
 
-    const wchar_t *utf16_result = utf8_to_utf16(utf8, strlen(utf8));
+    const char16_t *utf16_result = utf8_to_utf16(utf8, strlen(utf8));
 
-    ASSERT_EQ(wcscmp(utf16_str, utf16_result), 0);
+    ASSERT_EQ(u16cmp(utf16_str, utf16_result), 0);
 
-    size_t expected_length = wcslen(utf16_str);
-    size_t actual_length = wcslen(utf16_result);
+    size_t expected_length = u16len(utf16_str);
+    size_t actual_length = u16len(utf16_result);
 
     ASSERT_EQ(expected_length, actual_length);
 }
 
 UTEST(UTFTest, TruncatedUTF8ToUTF16) {
-    const wchar_t utf16_str[] = L"中文";
+    const char16_t utf16_str[] = u"中文";
     const char *utf8= u8"中文ABCD";
 
-    const wchar_t *utf16_result = utf8_to_utf16(utf8, strlen(utf8) - 4);
+    const char16_t *utf16_result = utf8_to_utf16(utf8, strlen(utf8) - 4);
 
-    ASSERT_EQ(wcscmp(utf16_str, utf16_result), 0);
+    ASSERT_EQ(u16cmp(utf16_str, utf16_result), 0);
 
-    size_t expected_length = wcslen(utf16_str);
-    size_t actual_length = wcslen(utf16_result);
+    size_t expected_length = u16len(utf16_str);
+    size_t actual_length = u16len(utf16_result);
 
     ASSERT_EQ(expected_length, actual_length);
 }
 
 UTEST(UTFTest, EmptyUTF8ToUTF16) {
-    const wchar_t utf16_str[] = L"";
+    const char16_t utf16_str[] = u"";
     const char *utf8= u8"";
 
-    const wchar_t *utf16_result = utf8_to_utf16(utf8, 0);
+    const char16_t *utf16_result = utf8_to_utf16(utf8, 0);
+    ASSERT_EQ(u16cmp(utf16_str, utf16_result), 0);
 
     ASSERT_EQ(utf16_result[0], 0);
 
 }
 
 UTEST(UTFTest, NullUTF8ToUTF16) {
-    const wchar_t *utf16_result = utf8_to_utf16(nullptr, 0);
+    const char16_t *utf16_result = utf8_to_utf16(nullptr, 0);
     ASSERT_EQ(utf16_result, nullptr);
 }
 
 UTEST(UTFTest, NormalUTF16ToUTF8) {
-    const wchar_t utf16_str[] = L"中文ABCD";
+    const char16_t utf16_str[] = u"中文ABCD";
 
     const char *utf8_expected = u8"中文ABCD";
 
@@ -63,7 +72,7 @@ UTEST(UTFTest, NormalUTF16ToUTF8) {
 }
 
 UTEST(UTFTest, EmptyUTF16ToUTF8) {
-    const wchar_t utf16_str[] = L"";
+    const char16_t utf16_str[] = u"";
 
     const char *utf8_expected = u8"";
 
@@ -84,7 +93,7 @@ UTEST(UTFTest, NullUTF16ToUTF8) {
 }
 
 UTEST(UTFTest, TruncatedUTF16ToUTF8) {
-    const wchar_t utf16_str[] = L"中文ABCD";
+    const char16_t utf16_str[] = u"中文ABCD";
 
     const char *utf8_expected = u8"中文AB";
 
