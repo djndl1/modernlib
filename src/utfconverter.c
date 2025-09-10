51cbec317c1830edf028e132b0a290802fffb385
djndl1 <djndl1@gmail.com>
2025-09-10 21:52:02 +0800

    feat(utfconverter): 处理大小端问题

--- src/utfconverter.c
+++ src/utfconverter.c
@@ -1,4 +1,5 @@
 #include "modernlib/dyn_char_array.h"
+#include <stdint.h>
 #include <stdlib.h>
 #include "modernlib/resmgmt.h"
 #include "modernlib/common_mem_ptr.h"
@@ -16,7 +17,7 @@ uint8_t *utf16_to_utf8(const uint16_t *wide_char_buffer, size_t buffer_len)
     int len_param = buffer_len < 1 ? -1 : buffer_len;
     int utf8_buffer_len = WideCharToMultiByte(CP_UTF8, 0, wide_char_buffer, len_param, nullptr, 0, nullptr, nullptr);
 
-    uint8_t *utf8_buffer = calloc(utf8_buffer_len + 1, sizeof(char));
+    char *utf8_buffer = calloc(utf8_buffer_len + 1, sizeof(char));
     if (utf8_buffer == nullptr) return nullptr;
 
     int result = WideCharToMultiByte(CP_UTF8, 0, wide_char_buffer, len_param, utf8_buffer, utf8_buffer_len + 1, nullptr, nullptr);
@@ -27,7 +28,7 @@ uint8_t *utf16_to_utf8(const uint16_t *wide_char_buffer, size_t buffer_len)
         return nullptr;
     }
 
-    return utf8_buffer;
+    return (uint8_t*)utf8_buffer;
 }
 
 uint16_t *utf8_to_utf16(const uint8_t *utf8_buffer, size_t buffer_len)
@@ -36,12 +37,12 @@ uint16_t *utf8_to_utf16(const uint8_t *utf8_buffer, size_t buffer_len)
 
     int len_param = buffer_len < 1 ? -1 : buffer_len;
 
-    int utf16_buffer_len = MultiByteToWideChar(CP_UTF8, 0, utf8_buffer, len_param, nullptr, 0);
+    int utf16_buffer_len = MultiByteToWideChar(CP_UTF8, 0, (const char*)utf8_buffer, len_param, nullptr, 0);
 
     uint16_t *utf16_buffer = calloc(utf16_buffer_len + 1, sizeof(wchar_t));
     if (utf16_buffer == nullptr) return nullptr;
 
-    int result = MultiByteToWideChar(CP_UTF8, 0, utf8_buffer, len_param, utf16_buffer, utf16_buffer_len + 1);
+    int result = MultiByteToWideChar(CP_UTF8, 0, (const char*)utf8_buffer, len_param, utf16_buffer, utf16_buffer_len + 1);
     if (result == 0) {
         if (utf16_buffer != nullptr) {
             free(utf16_buffer);
@@ -54,6 +55,15 @@ uint16_t *utf8_to_utf16(const uint8_t *utf8_buffer, size_t buffer_len)
 
 #else
 #include <iconv.h>
+
+bool is_little_endian(void)
+{
+    const uint16_t word = 0x00FF;
+
+    const char *bytes = (const char*)&word;
+
+    return bytes[0] == 0xFF;
+}
 
 char *iconv_convert(const char *source_charset, const char *dest_charset, const char *source_buffer, size_t buffer_len, size_t output_buffer_init_size, size_t output_cp_size)
 {
@@ -118,8 +128,9 @@ uint16_t *utf8_to_utf16(const uint8_t *utf8_buffer, size_t buffer_len)
     if (buffer_len < 0) {
         buffer_len = strlen((const char*)utf8_buffer);
     }
+    const char *utf16name = is_little_endian() ? "UTF-16LE" : "UTF-16BE";
     const char *input_buffer = (const char*)utf8_buffer;
-    char *output = iconv_convert("UTF-8", "UTF-16LE", input_buffer, buffer_len, 2 * buffer_len, 1);
+    char *output = iconv_convert("UTF-8", utf16name, input_buffer, buffer_len, 2 * buffer_len, 1);
 
     return (uint16_t*)output;
 }
@@ -139,10 +150,11 @@ uint8_t *utf16_to_utf8(const uint16_t *utf16_buffer, size_t buffer_len)
     if (buffer_len < 0) {
         buffer_len = u16len(utf16_buffer);
     }
+    const char *utf16name = is_little_endian() ? "UTF-16LE" : "UTF-16BE";
     const char *input_buffer = (const char*)utf16_buffer;
-    char *output = iconv_convert("UTF-16LE", "UTF-8", input_buffer, buffer_len * 2, buffer_len + buffer_len / 2 + 4, 2);
+    char *output = iconv_convert(utf16name, "UTF-8", input_buffer, buffer_len * 2, buffer_len + buffer_len / 2 + 4, 2);
     if (output == nullptr) {
-        output = iconv_convert("UTF-16LE", "UTF-8", input_buffer, buffer_len * 2, 2 * buffer_len, 2);
+        output = iconv_convert(utf16name, "UTF-8", input_buffer, buffer_len * 2, 2 * buffer_len, 2);
     }
 
     return (uint8_t*)output;
